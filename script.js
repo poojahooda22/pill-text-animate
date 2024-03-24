30de393cf477392000003470871ef00b04aed208
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-24 20:48:14 +0530

    updated random color

--- script.js
+++ script.js
@@ -89,6 +89,7 @@ class Word {
     constructor(x, y, word) {
         this.body = Bodies.rectangle(x,y,word.length * 20, 40);
         this.word = word;
+        //for random colors
         this.color = '#' + Math.floor(Math.random()*16777215).toString(16);
         World.add(engine.world, this.body);
     }
