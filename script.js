d5a4ace33f0cc7c78b4354c64e54951ea6b11021
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-24 21:16:16 +0530

    add

--- script.js
+++ script.js
@@ -110,7 +110,6 @@ class Word {
 
         //apply  the color
         fill(this.color); 
-        
         strokeWeight(0);
         rect(0, 0, this.word.length * 20, 60, 30);
         noStroke();
